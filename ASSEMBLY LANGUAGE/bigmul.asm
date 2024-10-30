cd7702a3a7622066cb812c0678d909f900aea780
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2024-10-30 13:23:35 +0600

    Add files via upload

---
+++ ASSEMBLY LANGUAGE/bigmul.asm
@@ -0,0 +1,36 @@
+.model small
+.stack 100h
+.data
+
+.code
+main proc
+    mov ax, @data
+    mov ds, ax
+
+    mov al, 10
+    mov bl, 4
+    mul bl
+    mov cx, 10
+    mov si, 0
+
+convert:
+    xor dx, dx
+    div cx
+    add dl, 30h
+    push dx
+    inc si
+    cmp ax, 0
+    jnz convert
+
+print:
+    mov ah, 02h
+print_loop:
+    pop dx
+    int 21h
+    dec si
+    jnz print_loop
+
+    mov ah, 4Ch
+    int 21h
+main endp
+end main
