10b60946d3973d9a220b9de72f86f81fe04d7322
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2024-11-18 21:46:02 +0600

    Create Sharpening Filter.py

---
+++ DIGITAL IMAGE PROCESSING/Sharpening Filter.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from google.colab import drive
+
+
+drive.mount("/content/drive", force_remount=True)
+
+
+arr = cv2.imread("/content/drive/MyDrive/Lab 1 DIP/face grayscale.jpg", cv2.IMREAD_GRAYSCALE)
+
+
+newarr = np.zeros_like(arr)
+
+
+kernel = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+kernel_sum = np.sum(kernel)
+
+
+for i in range(1, arr.shape[0] - 1):
+    for j in range(1, arr.shape[1] - 1):
+        region = arr[i - 1: i + 2, j - 1: j + 2]
+        newarr[i, j] = np.sum(region * kernel) / kernel_sum
+
+finalarr = arr + (arr - newarr);
+plt.subplot(1, 3, 1)
+plt.imshow(arr, cmap='gray')
+plt.title('Original Image')
+
+plt.subplot(1, 3, 2)
+plt.imshow(newarr, cmap='gray')
+plt.title('Blur Image')
+
+plt.subplot(1, 3, 3)
+plt.imshow(finalarr, cmap='gray')
+plt.title('Sharp Image')
+
+plt.show()
