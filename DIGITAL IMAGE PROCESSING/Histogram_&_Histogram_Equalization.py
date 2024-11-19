cf299ef4b95e0c4fd372b78e1723e60ffb290a41
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2024-11-19 23:26:16 +0600

    Update Histogram_&_Histogram_Equalization.py

--- DIGITAL IMAGE PROCESSING/Histogram_&_Histogram_Equalization.py
+++ DIGITAL IMAGE PROCESSING/Histogram_&_Histogram_Equalization.py
@@ -7,18 +7,7 @@ arr = cv2.imread("/content/drive/MyDrive/Lab 1 DIP/histrogram.png", cv2.IMREAD_G
 finalarr = cv2.imread("/content/drive/MyDrive/Lab 1 DIP/histrogram.png", cv2.IMREAD_GRAYSCALE)
 
 
-#arr = np.array([[0,1,2,4], [0,0,3,5], [0,0,2,3],[1,7,1,1]])
-#arr
-
-mx = int(-1e9);
-
-height, width = arr.shape[:2]
-for i in range(height):
-    for j in range(width):
-        if mx < arr[i][j]:
-            mx = arr[i][j]
-
-print(mx)
+mx = np.max(arr)
 
 newarr = [0] * (mx + 1)
 
@@ -29,10 +18,6 @@ for i in range(height):
     for j in range(width):
         newarr[arr[i][j]] += 1
 
-for i in range(mx + 1):
-    print(newarr[i], end=" ")
-
-print("\n");
 
 pdf = [0] * (mx + 1)
 
@@ -40,10 +25,6 @@ length = height * width
 for i in range(mx + 1):
     pdf[i] = newarr[i] / length
 
-for i in range(mx + 1):
-    print(pdf[i], end=" ")
-
-print("\n");
 
 cdf = [0] * (mx + 1)
 cdf[0] = pdf[0]
@@ -51,22 +32,14 @@ cdf[0] = pdf[0]
 for i in range(1, mx + 1):
     cdf[i] = cdf[i - 1] + pdf[i]
 
-for i in range(mx + 1):
-    print(cdf[i], end=" ")
 
-print("\n");
 
 len = mx
 for i in range(len):
     x = cdf[i] * mx
     cdf[i] = round(x)
 
-for i in range(mx + 1):
-    print(cdf[i], end=" ")
 
-print("\n");
-
-#finalarr = np.zeros_like(arr)
 
 for i in range(height):
     for j in range(width):
@@ -74,7 +47,6 @@ for i in range(height):
 
 
 
-print(finalarr);
 
 plt.figure(figsize=(10, 10))
 plt.bar(np.arange(256), newarr)
